acf611a4ea62195a94365e0a644dc493dbac8e64
Stephen Belyea <belyea.stephen@gmail.com>
2019-07-24 19:57:58 -0400

    Install stylish and sample styling

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import { string } from "prop-types";
+import stylish from "@dmamills/stylish";
+
+const styles = stylish({
+  border: "solid .1rem royalblue",
+  padding: ".5rem 1rem",
+  "font-weight": "bold",
+  background: "royalblue",
+  "border-radius": ".25rem",
+  color: "#fff",
+  cursor: "pointer",
+  transition: "background 150ms, border 150ms",
+  ":hover, :focus": {
+    background: "navy",
+    "border-color": "navy"
+  }
+});
 
 function Button({ children, ...props }) {
-  return <button {...props}>{children}</button>;
+  return (
+    <button {...props} className={styles}>
+      {children}
+    </button>
+  );
 }
 
 Button.propTypes = {
